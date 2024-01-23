b4a1635028b8e0a664463aa377e735fdf4743568
Vittorio Bartolozzi <vittorio.bartolozzigmail.com>
2024-01-23 22:48:06 +0100

    Spring Initialization

---
+++ src/main/java/com/bartolozzi/ManagingEmployees/ManagingEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.bartolozzi.ManagingEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartolozzi/ManagingEmployees/ManagingEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartolozzi.ManagingEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
